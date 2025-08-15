a76127b1e6e798a83d71d751aa235c8f74deb5a8
Eva <evaccorvera@gmail.com>
2025-08-15 01:09:07 -0600

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Tripleten web_project_around
+Este proyecto es un ejemplo de perfil editable usando un popup modal en HTML, CSS y JavaScript.
---
+++ script.js
@@ -0,0 +1,28 @@
+const openBtn = document.querySelector(".main__button_edit");
+const popup = document.querySelector(".popup");
+const closeBtn = document.querySelector(".popup__button_close");
+const form = document.querySelector(".popup__container");
+const inputName = document.querySelector(".popup__input_name");
+const inputAbout = document.querySelector(".popup__input_about");
+
+const profileName = document.querySelector(".main__paragraph_name");
+const profileAbout = document.querySelector(".main__paragraph_about");
+
+openBtn.addEventListener("click", () => {
+  popup.classList.remove("popup__hidden");
+
+inputName.value = profileName.textContent;
+inputAbout.value = profileAbout.textContent;
+});
+
+closeBtn.addEventListener("click", () => {
+  popup.classList.add("popup__hidden");
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  profileName.textContent = inputName.value;
+  profileAbout.textContent = inputAbout.value;
+
+  popup.classList.add("popup__hidden");
+});
